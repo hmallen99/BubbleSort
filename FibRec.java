3b415ae17a4efb79a787ff17cbe0e955bc21dd12
hmallen99 <31718268+hmallen99@users.noreply.github.com>
2017-09-06 21:26:19 -0700

    Add files via upload

---
+++ FibRec.java
@@ -0,0 +1,19 @@
+
+public class FibRec {
+	public static void main(String[] args){
+		System.out.print(fib(8));
+	}
+	
+	public static int fib(int n){
+		if(n==0){
+			return 0;
+		}
+		else if(n==1){
+			return 1;
+		}
+		else{
+			return fib(n-2)+fib(n-1);
+		}
+	}
+	
+}
